558bbd93b99f002d674bd3c56573dec6d8b364ef
SousaPedrop <sousapedro.work@gmail.com>
2023-02-21 17:42:20 -0300

    Conclusão do Desafio 02

--- SegundoDia.cs
+++ SegundoDia.cs
@@ -9,8 +9,39 @@ namespace DesafioBaltaCarnaval
     {
         public static void CalcularIMC()
         {
-            System.Console.WriteLine("Em formação...");
-            Console.ReadKey();
+            Console.Write("Digite o peso: ");
+            double.TryParse(Console.ReadLine(), out double peso);
+            Console.Write("Digite a altura: ");
+            double.TryParse(Console.ReadLine(), out double altura);
+            if (peso > 0 && altura > 0)
+            {
+                double Imc = peso / Math.Pow(altura, 2);
+                Console.WriteLine($"Seu IMC é: {Imc.ToString("N")}");
+                if (Imc >= 25 && Imc <= 29.9)
+                {
+                    Console.WriteLine($"Sobrepeso");
+                    Console.WriteLine($"Risco: Aumentado");
+                }
+                else if(Imc >= 30 && Imc <= 34.9){
+                    Console.WriteLine($"Obesidade Grau I");
+                    Console.WriteLine($"Risco: Moderado");
+                }
+                else if(Imc >= 35 && Imc <= 40){
+                    Console.WriteLine($"Obesidade Grau II");
+                    Console.WriteLine($"Risco: Grave");
+                }
+                else if(Imc > 40)
+                {
+                    Console.WriteLine($"Obesidade Grau III");
+                    Console.WriteLine($"Risco: Muito Grave");
+                }
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("Digite peso e altura válidos.");
+                Console.ReadKey();
+            }
         }
     }
 }

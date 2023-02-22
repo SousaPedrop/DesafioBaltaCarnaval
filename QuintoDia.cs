bc721370bdf2cfd54325c7a05a0ca6132d1cb9fa
SousaPedrop <sousapedro.work@gmail.com>
2023-02-22 20:24:57 -0300

    Criando Arquivos para desafios 05 e 06

---
+++ QuintoDia.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DesafioBaltaCarnaval
+{
+    public static class QuintoDia
+    {
+        public static void ValorPorExtenso()
+        {
+            Console.WriteLine("Em Desenvolvimento...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SextoDia.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DesafioBaltaCarnaval
+{
+    public static class SextoDia
+    {
+        public static void EncriptarTexto()
+        {
+            Console.WriteLine("Em Desenvolvimento...");
+            Console.ReadKey();
+        }
+    }
+}

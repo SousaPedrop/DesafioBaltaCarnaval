e967182cc1e35234a47e1819743195b674297c45
SousaPedrop <sousapedro.work@gmail.com>
2023-02-22 20:25:34 -0300

    Desafio 03 Concluído

--- TerceiroDia.cs
+++ TerceiroDia.cs
@@ -9,7 +9,22 @@ namespace DesafioBaltaCarnaval
     {
         public static void EscolherGasolinaOuAlcool()
         {
-            System.Console.WriteLine("Em formação...");
+            Console.Write("Digite o valor do Alcool/Etanol: ");
+            double.TryParse(Console.ReadLine(), out double PrecoEtanol);
+            Console.Write("Digite o valor da Gasolina: ");
+            double.TryParse(Console.ReadLine(), out double PrecoGasolina);
+            if(PrecoEtanol > 0 && PrecoGasolina > 0) 
+            {
+                double comparacao = PrecoEtanol / PrecoGasolina;
+                if (comparacao > 0.65)
+                {
+                    Console.WriteLine("É mais vantajoso abastercer com Alcool/Etanol.");
+                }
+                else
+                {
+                    Console.WriteLine("É mais vantajoso abastercer com Gasolina."); 
+                }
+            }
             Console.ReadKey();
         }
     }

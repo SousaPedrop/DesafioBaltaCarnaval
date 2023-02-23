4ce9c722634a5f5ffeece2956c6b8a7cebabf33b
SousaPedrop <sousapedro.work@gmail.com>
2023-02-23 16:57:00 -0300

    Criando a classe para o Desafio 07

---
+++ SetimoDia.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DesafioBaltaCarnaval
+{
+    public static class SetimoDia
+    {
+        public static void PiramideFibonacci()
+        {
+            Console.WriteLine("Em desenvolvimento...");
+            Console.ReadKey();
+        }
+    }
+}

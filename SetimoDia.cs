1cfb09fa328050352599fe22ed9892ce562a1482
SousaPedrop <sousapedro.work@gmail.com>
2023-02-24 23:16:46 -0300

    Desafio 07 concluído

--- SetimoDia.cs
+++ SetimoDia.cs
@@ -9,7 +9,24 @@ namespace DesafioBaltaCarnaval
     {
         public static void PiramideFibonacci()
         {
-            Console.WriteLine("Em desenvolvimento...");
+            Console.Clear();
+            List<int> sequenciaFibonacci = new List<int>();
+            string sequenciaFibonacciString = "";
+            int proximoValor = 0;
+            for (int i = 0; i <= 20; i++)
+            {
+                sequenciaFibonacci.Add(proximoValor);
+                if (i > 0)
+                {
+                    proximoValor = sequenciaFibonacci[i] + sequenciaFibonacci[i-1];
+                }
+                else
+                {
+                    proximoValor = sequenciaFibonacci[i] + 1;
+                }
+                sequenciaFibonacciString += $"{sequenciaFibonacci[i]} ";
+                Console.WriteLine(sequenciaFibonacciString);
+            }
             Console.ReadKey();
         }
     }

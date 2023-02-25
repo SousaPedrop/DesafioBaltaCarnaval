1e9d92661a7aa0c0164d5c1a5b82e93cf533632e
SousaPedrop <sousapedro.work@gmail.com>
2023-02-24 22:40:19 -0300

    Desafio 06 concluído

--- SextoDia.cs
+++ SextoDia.cs
@@ -9,7 +9,31 @@ namespace DesafioBaltaCarnaval
     {
         public static void EncriptarTexto()
         {
-            Console.WriteLine("Em Desenvolvimento...");
+            Dictionary<char, char> cript = new Dictionary<char, char>
+                {
+                {'A','F'}, {'B','G'}, {'C','H'}, {'D','I'}, {'E','J'}, {'F','K'}, {'G','L'}, {'H','M'}, {'I','N'},
+                {'J','O'}, {'K','P'}, {'L','Q'}, {'M','R'}, {'N','S'}, {'O','T'}, {'P','U'}, {'Q','V'}, {'R','W'},
+                {'S','X'}, {'T','Y'}, {'U','Z'}, {'V','Ç'}, {'W','A'}, {'X','B'}, {'Y','C'}, {'Z','D'}, {'Ç','E'},
+                {' ',' '}, {'.','.'}, {',',','}, {';',';'}, {':',':'}, {'!','!'}, {'?','?'}, {'-','-'}, {'"','"'},
+                };
+            
+            Console.WriteLine("Digite o texto para criptografar: ");
+            string? entrada = Console.ReadLine();
+            if (String.IsNullOrEmpty(entrada))
+            {
+                Console.WriteLine("Entrada vazia ou nula. Digite uma entrada válida.");
+            }
+            else
+            {
+                string saida = "";
+                entrada = entrada.ToUpper();
+                for(int i = 0; i < entrada.Length; i++)
+                {
+                    saida += cript.GetValueOrDefault(entrada[i]);
+                }
+                Console.WriteLine("Resultado:\n");
+                Console.WriteLine(saida);
+            }
             Console.ReadKey();
         }
     }
